ff72800f7fab3b5230874064bef665fcc96c88e3
Sam5196 <sammedpachore1008@gmail.com>
2022-08-29 18:37:20 +0530

    Added:Using function calculating Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,34 +1,23 @@
 console.log("Welcome to Employee Wage Program")
-//UC1--> Employee is Present or not
-{
-    const IS_PRESENT=1;
-    let empcheck=Math.floor(Math.random()*2);
-    if(empcheck==IS_PRESENT){
-        console.log("Employee is Present");
-    }else{
-        console.log("Employee is Absent");
-    }
-}
-//UC2-->Calculating Employee Wage
 {
     const IS_PART_TIME=1;
     const IS_FULL_TIME=2;
     const PART_TIME_HOURS=4;
     const FULL_TIME_HOURS=8;
     const WAGE_PER_HOUR=20;
-    let empHr=0;
-    empCheck=Math.floor(Math.random()*3);
-    switch (empCheck) {
-        case IS_PART_TIME:
-            empHr=PART_TIME_HOURS;            
-            break;
-        case IS_FULL_TIME:
-            empHr=FULL_TIME_HOURS;
-            break
-        default:
-            empHr=0;
-            break;
+    function getWorkingHours(empCheck){
+        switch(empCheck){
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
     }
+    let empHr=0;
+    let empCheck=Math.floor(Math.random()*3);
+    empHr=getWorkingHours(empCheck);
     let empWage=empHr*WAGE_PER_HOUR;
-    console.log(" UC2-->Employee Wage is :"+empWage);
+    console.log("Employee Daily wage :"+empWage);
 }
