b4316c6541fb91e4662dd9fbeef13dc486ccaf06
Sam5196 <sammedpachore1008@gmail.com>
2022-08-29 18:40:05 +0530

    Added:Calculating Monthly Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,5 +1,6 @@
 console.log("Welcome to Employee Wage Program")
 {
+    const NUM_OF_WORKING_DAYS=20;
     const IS_PART_TIME=1;
     const IS_FULL_TIME=2;
     const PART_TIME_HOURS=4;
@@ -16,8 +17,10 @@ console.log("Welcome to Employee Wage Program")
         }
     }
     let empHr=0;
-    let empCheck=Math.floor(Math.random()*3);
-    empHr=getWorkingHours(empCheck);
+    for(let day =0;day<NUM_OF_WORKING_DAYS;day++){
+        let empCheck=Math.floor(Math.random()*3);
+        empHr+=getWorkingHours(empCheck);
+    }
     let empWage=empHr*WAGE_PER_HOUR;
-    console.log("Employee Daily wage :"+empWage);
+    console.log("Total Hours "+empHr+" Employee Wage is :"+empWage);
 }
